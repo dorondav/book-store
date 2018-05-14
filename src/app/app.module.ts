89a81daf1a332f794fa4a7bb6e88b1597fbdc82b
Doron Davidowitz <dorondavidowitz@gmail.com>
2018-05-14 19:38:37 +0300

    site structure completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { BookEditComponent } from './books/book-edit/book-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { BookComponent } from './books/book/book.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    BookEditComponent,
+    HeaderComponent,
+    BookComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule
